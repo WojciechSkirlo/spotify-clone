e07cd970c12acca8aebaabd05f7cf5871f978b32
Wojciech Skirło <wojciechskiro@gmail.com>
2023-04-26 22:31:28 +0200

    feat: add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import BaseButton from "./components/BaseButton";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        {/* <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
-        Spotify
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <BaseButton />
+            <h1>Vite + React</h1>
+        </>
+    );
 }
 
-export default App
+export default App;
