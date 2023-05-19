6235a61036dc950e6f151e762c31a9d584fd4880
Wojciech Skirło <wojciechskiro@gmail.com>
2023-05-19 22:33:00 +0200

    feat(fonts): add font-family CircularSt

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./routes/root";
-import Contact from "./routes/contact";
-import ErrorPage from "./error-page";
 import "./index.css";
+
+import DefaultLayout from "./layouts/default";
+import Home from "./pages/index";
+import Search from "./pages/search";
+import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Root />,
+        element: <DefaultLayout />,
         errorElement: <ErrorPage />,
         children: [
             {
-                path: "contacts/:contactId",
-                element: <Contact />,
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: "search",
+                element: <Search />,
             },
         ],
     },
