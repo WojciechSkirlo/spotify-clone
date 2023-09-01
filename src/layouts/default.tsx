cdf3253da8247bd07ded642c5daa22d5d70dfaad
Mateusz Kapuściński <kapuscinski.mateusz01@gmail.com>
2023-09-01 22:26:30 +0200

    feat(layout): add initial layout

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -4,14 +4,15 @@ import TheNavigation from '../components/the/Navigation';
 
 export default function Root() {
   return (
-    <>
-      <div>
+    <div className="grid h-full grid-cols-4 grid-rows-1 gap-2 p-2">
+      <aside className="flex flex-col gap-2">
         <TheNavigation />
-        <div>
-          <Outlet />
-        </div>
-      </div>
+        <div className="px-6 py-2 rounded-lg bg-dark-gray grow">Your Library</div>
+      </aside>
+      <main className="col-span-3 px-6 rounded-lg bg-dark-gray">
+        <Outlet />
+      </main>
       <TheFooter />
-    </>
+    </div>
   );
 }
