7bedc89f1d67317062c0d210694e6682a2d2f2e4
Wojciech Skirło <wojciechskiro@gmail.com>
2023-09-02 12:44:49 +0200

    feat!: update fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontSize: {
+      xs: ['0.6875rem', '1.125rem'], // 11px
+      sm: ['0.875rem', '1.375rem'], // 14px
+      base: ['1rem', '1.625rem'], // 16px
+      lg: ['1.125rem', '1.125rem'], // 18px
+      xl: ['1.5rem', '1.5rem'], // 24px,
+      '2xl': ['4.5rem', '4.5rem'] // 72px
+    },
     extend: {
       colors: {
         'dark-gray': '#121212',
