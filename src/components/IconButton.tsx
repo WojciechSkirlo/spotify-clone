f2649050faa8c1517d0e24180a9749fff1908fca
Wojciech Skirło <wojciechskiro@gmail.com>
2023-09-05 19:18:37 +0200

    feat(footer-controls): set appropriate icons

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -2,11 +2,16 @@ import Icon from '~~/Icon';
 
 type IconButtonProps = {
   icon: string;
+  ariaLabel?: string;
 };
 
-const IconButton = ({ icon }: IconButtonProps) => {
+const IconButton = ({ icon, ariaLabel }: IconButtonProps) => {
   return (
-    <button type="button" className="flex items-center justify-center w-8 h-8 text-subtle hover:text-white">
+    <button
+      type="button"
+      aria-label={ariaLabel}
+      className="flex items-center justify-center w-8 h-8 text-subtle hover:text-white"
+    >
       <Icon name={icon} />
     </button>
   );
--- src/layouts/Footer/Controls.tsx
+++ src/layouts/Footer/Controls.tsx
@@ -1,5 +1,16 @@
+import IconButton from '~~/IconButton';
+
 const Controls = () => {
-  return <div className="flex items-center justify-center col-span-3">controls</div>;
+  return (
+    <div className="flex items-center justify-end col-span-3">
+      <IconButton icon="card-play" />
+      <IconButton icon="microphone" />
+      <IconButton icon="queue" />
+      <IconButton icon="device" />
+      <IconButton icon="volume" />
+      <IconButton icon="maximize" />
+    </div>
+  );
 };
 
 export default Controls;
