beb763ae2710d37027ebe8a0e46e8770d9c65991
Mateusz Kapuściński <kapuscinski.mateusz01@gmail.com>
2023-09-06 19:12:42 +0200

    feat(icons): add ability to set Icon component size using props

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,12 +1,28 @@
+type Sizes = 'sm' | 'md' | 'lg';
+
 type IconProps = {
   name: string;
+  size?: Sizes;
 };
+
+const defaultProps: Omit<IconProps, 'name'> = {
+  size: 'md'
+};
+
 const icons = import.meta.glob('../assets/icons/*.svg', { as: 'raw', eager: true });
 
-const Icon = ({ name }: IconProps) => {
+const Icon = ({ name, size }: IconProps) => {
   const selectedIcon = icons[`../assets/icons/${name}.svg`];
 
-  return <div className="w-4 h-4 fill-current" dangerouslySetInnerHTML={{ __html: selectedIcon }}></div>;
+  const sizes: Record<Sizes, string> = {
+    sm: 'w-2 h-2',
+    md: 'w-4 h-4',
+    lg: 'w-6 h-6'
+  };
+
+  return <div className={`fill-current ${sizes[size!]}`} dangerouslySetInnerHTML={{ __html: selectedIcon }}></div>;
 };
+
+Icon.defaultProps = defaultProps;
 
 export default Icon;
