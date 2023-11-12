99a71dd04679c6839160b9373b86b0e49c3fdca4
Wojciech Skirło <wojciechskiro@gmail.com>
2023-11-12 14:05:26 +0100

    feat: add device id to player store

--- src/context/player.ts
+++ src/context/player.ts
@@ -1,25 +1,29 @@
 import { create } from 'zustand';
 import PlayerService from '@/services/player';
 
+type State = {
+  deviceId: string;
+};
+
 type Action = {
   play: (context_uri: string, position?: number) => void;
   pause: () => void;
   shuffle: (state: boolean) => void;
   repeat: (repeatMode: number) => void;
+  setDeviceId: (deviceId: string) => void;
 };
 
-const usePlayerStore = create<Action>(() => ({
+const usePlayerStore = create<State & Action>((set, get) => ({
+  deviceId: '',
+
   play: (context_uri, position) => {
     const data: PlayData = {
       context_uri,
       position_ms: 0
     };
 
-    if (position) {
-      data.offset = { position };
-    }
-
-    PlayerService.play(data);
+    if (position) data.offset = { position };
+    PlayerService.play(data, get().deviceId);
   },
 
   pause: () => {
@@ -35,6 +39,10 @@ const usePlayerStore = create<Action>(() => ({
     const newMode = currentRepeatMode === 0 ? 1 : currentRepeatMode === 1 ? 2 : 0;
 
     PlayerService.repeat(modes[newMode]);
+  },
+
+  setDeviceId: (deviceId: string) => {
+    set({ deviceId });
   }
 }));
 
